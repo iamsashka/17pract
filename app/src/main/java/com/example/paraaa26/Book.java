0e5f118ab461491a60550982bd2d904c11bb8c22
sashka <144311184+iamsashka@users.noreply.github.com>
2024-11-27 09:20:00 +0300

    халява приди

---
+++ app/src/main/java/com/example/paraaa26/Book.java
@@ -0,0 +1,42 @@
+package com.example.paraaa26;
+public class Book {
+    private String id;
+    private String title;
+    private String author;
+    private String description;
+
+    public Book (String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.author= author;
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
